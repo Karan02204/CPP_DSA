3404069591af7575b07794857612c3938a9f62af
{Karan <{karanattri022@gmail.com}>
2024-08-22 19:58:52 +0530

    squaring  number

---
+++ squaring.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number you want to get a square of :" << endl;
+    cin >> n;
+    int square;
+    square=n*n;
+    cout << "The Squared number is : "<<square;
+}
