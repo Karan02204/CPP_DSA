d459c90c696295ac06d40ab8ecf28522a2f055af
{Karan <{karanattri022@gmail.com}>
2024-08-23 20:04:08 +0530

    Binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int binarySearch(int arr[],int size,int key){
     int low = 0;
     int high = size - 1;
-    int mid=(low+high)/2;
+    
 
     while (low<=high){
+        int mid=(low+high)/2;
         if (arr[mid] == key){
             return mid;
         }
@@ -16,12 +17,11 @@ int binarySearch(int arr[],int size,int key){
         else {
             high = mid - 1;
         }
-        mid=(low+high)/2;
     }
 
     return -1;
 }
 int main(){
     int arr[5]={2,3,4,5,6};
-    cout <<"Index of 3 is " << binarySearch(arr,5,13);
+    cout <<"Index of 3 is " << binarySearch(arr,5,3);
 }
