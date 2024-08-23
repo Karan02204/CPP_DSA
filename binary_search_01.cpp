8885065c11f44dd0f16be7ef362b394113e41e9e
{Karan <{karanattri022@gmail.com}>
2024-08-23 20:58:44 +0530

    New

--- binary_search_01.cpp
+++ binary_search_01.cpp
@@ -5,13 +5,14 @@ int binarySearch(int arr[],int size,int key){
     int low = 0;
     int high = size - 1;
     
-
+    
     while (low<=high){
+        int ans=-1;
         int mid=(low+high)/2;
         if (arr[mid] == key){
-            if(arr[mid-1]!=key){
-                return mid;
-            }
+            ans=mid;
+            high=mid-1;
+            return mid;
         }
         if (key>arr[mid]){
             low = mid + 1;
