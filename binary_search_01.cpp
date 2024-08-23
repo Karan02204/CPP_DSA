53cd05540c45174ca9d33b13f164293087a6a3a0
{Karan <{karanattri022@gmail.com}>
2024-08-23 20:15:31 +0530

    binary search question

---
+++ binary_search_01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[],int size,int key){
+    int low = 0;
+    int high = size - 1;
+    
+
+    while (low<=high){
+        int mid=(low+high)/2;
+        if (arr[mid] == key){
+            if(arr[mid-1]!=key){
+                return mid;
+            }
+        }
+        if (key>arr[mid]){
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+int main(){
+    int arr[7]={1,2,3,3,3,3,5};
+    cout <<"Index of 3 is " << binarySearch(arr,7,3);
+}
