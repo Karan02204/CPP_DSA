4cc976b5b333a4d858a2e9a6eee829c251ee6f09
{Karan <{karanattri022@gmail.com}>
2024-08-26 19:22:36 +0530

    First question day 1
    Author:    Karan02204 <karanattri022@gmail.com>

--- Day 1/1.cpp
+++ Day 1/1.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    cout << "Enter the first String: ";
+    string str1;
+    cin >> str1;
+    cout << "Enter the second String: ";
+    string str2;
+    cin >> str2;
+    cout << "Concatenated string :" <<str1 << str2;
+}
