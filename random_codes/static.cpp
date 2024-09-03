dd79035a91e81833b2e6a9657c29eddebb5b2935
{Karan <{karanattri022@gmail.com}>
2024-09-03 20:39:37 +0530

    random codes

---
+++ random_codes/static.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Bank{
+    public:
+    int accNumber;
+    int balance;
+    static int numberOfAccounts;
+
+    Bank(){
+
+    }
+
+    Bank(int accNumber, int balance){
+        this->accNumber = accNumber;
+        this->balance = balance;
+    }
+
+    void deposite(int amount);
+
+    static int getNumberOfAccounts();
+};
+
+int Bank::numberOfAccounts = 0;
+
+void Bank::deposite(int amount){
+    balance += amount;
+}
+
+int Bank::getNumberOfAccounts(){
+    return numberOfAccounts;
+}
+
+int main(){
+    cout << Bank::getNumberOfAccounts(); 
+}
