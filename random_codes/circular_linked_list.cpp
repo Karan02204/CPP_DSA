e14e053aa11d3e5cc19a34dd2b492db0bf7e7dd3
Karan02204 <karanattri022@gmail.com>
2024-09-13 19:08:05 +0530

    random codes

--- random_codes/circular_linked_list.cpp
+++ random_codes/circular_linked_list.cpp
@@ -11,34 +11,127 @@ class Node{
     }
 };
 
-void insertatTail(Node* &head,int val){
+void insertatHead(Node* &head, int val){
     Node* n = new Node(val);
-    if(head == NULL){
-        head = n;
+
+    if(head==NULL){
+        n->next=n;
+        head=n;
         return;
     }
 
-    Node* temp = head;
+    Node* temp=head;
 
-    while(temp->next != head){
-        temp = temp->next;
+    while(temp->next !=head){
+        temp=temp->next;
     }
 
-    temp->next = n;
+    temp->next=n;
+    n->next=head;
+    head=n;
+}
+
+void insertatTail(Node* &head,int val){
+    Node* n = new Node(val);
+
+    if(head==NULL){
+        insertatHead(head,val);
+        return;
+    }
+
+    Node* temp=head;
+
+    while(temp->next!=head){
+        temp=temp->next;
+    }
+
+    temp->next=n;
     n->next=head;
 }
 
-void display(Node* head){
-
-    Node* temp = head;
-
-    while(temp!=NULL){
-        cout << temp->data << " -> ";
-        temp = temp->next;
+Node* floydDetectLoop(Node* head){
+    if(head==NULL){
+        return NULL;
     }
-    cout << "NULL" << endl;
 
-    
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast!=NULL && fast->next!=NULL){
+        slow = slow->next;
+        fast=fast->next->next;
+
+        if(slow == fast) return slow;
+    }
+
+    return NULL;
+}
+
+void deleteatHead(Node* &head){
+    Node* temp=head;
+
+    while(temp->next!=head){
+        temp=temp->next;
+    }
+
+    Node* toDel=head;
+    temp->next=head->next;
+    head=head->next;
+
+    delete toDel;
+}
+
+void display(Node* head){
+    Node* temp=head;
+
+    do{
+        cout << temp->data << "->";
+        temp=temp->next;
+    }
+
+    while(temp!=head);
+}
+
+
+Node* getStartingNode(Node* head){
+    if(head == NULL) return NULL;
+
+    Node* intersection = floydDetectLoop(head);
+    Node* slow=head;
+
+    while(slow!=intersection){
+        slow= slow->next;
+        intersection = intersection->next;
+    }
+
+    return slow;
+}
+
+void removeLoop(Node* head){
+    if(head == NULL) return;
+
+    Node* startofLoop = getStartingNode(head);
+    Node* temp = startofLoop;
+
+    while(temp->next !=startofLoop){
+        temp=temp->next;
+    }
+
+    temp->next=NULL;
+}
+
+Node* reverse(Node* &head){
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* next;
+
+    while(curr!=NULL){
+        next = curr->next;
+        curr->next = prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
 }
 
 int main()
@@ -47,5 +140,15 @@ int main()
 
     insertatTail(head,1);
     insertatTail(head,2);
+    insertatTail(head,3);
+    insertatTail(head,4);
+
     display(head);
+    cout << endl;
+
+    if(floydDetectLoop(head)){
+        cout << "LOOP" << endl;
+    }
+
+    else cout << "NO LOOP" << endl;
 }
