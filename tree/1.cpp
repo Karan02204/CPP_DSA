4c56bb7ba643c2c231a7f5feb977bae38cce0fc5
Karan02204 <karanattri022@gmail.com>
2024-10-17 10:38:27 +0530

    tree

---
+++ tree/1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    Node* right;
+    Node* left;
+    int data;
+
+    Node(int val){
+        data = val;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+/*
+preoder -> root , left subtree , right subtree
+*/
+
+void preOrder(Node* root){
+    if(root == NULL) return ;
+
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void postOrder(Node* root){
+    if(root == NULL) return ;
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+void inOrder(Node* root){
+    if(root == NULL) return ;
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+int main()
+{
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right= new Node(7);
+
+    preOrder(root);
+
+    cout << endl;
+
+    inOrder(root);
+
+    cout << endl;
+
+    postOrder(root);
+}
---
+++ tree/2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <Queue>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printLevelOrder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp !=NULL){
+            cout << temp->data << " ";
+            if(temp->left != NULL){
+                q.push(temp->left);
+            }
+            if(temp->right != NULL){
+                q.push(temp->right);
+            }
+        }
+
+        else if(!q.empty()){
+            q.push(NULL);
+        }
+    }
+}
+
+void sumatlevel(Node* root ,int k){
+    int count = 0;
+    int sum = 0;
+    
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        
+        if(temp==NULL){
+            count++;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+            continue;
+        }
+
+        if(count == k-1){
+            sum += temp->data;
+        }
+
+        if(temp->left) q.push(temp->left);
+        if(temp->right) q.push(temp->right);
+
+        if(count>=k) break;
+    }
+
+    cout << sum << endl;
+}
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right= new Node(7);
+
+    sumatlevel(root,3);
+}
